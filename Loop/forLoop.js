4b07c22003a0ff95d88f3df697416879c95a2a65
obitosd <sahilkumar.sk202000@gmail.com>
2024-02-23 13:04:22 +0530

    odd even for loop questions

---
+++ Loop/forLoop.js
@@ -0,0 +1,42 @@
+let mayank=[1,2,4,5,7,8];
+
+
+function loveUBaby(){
+        for(i=0; i<=100; i++){
+                console.log("sahil")
+            }
+        }
+        loveUBaby()
+        
+function sahil(){
+    for(i=mayank.length; i>=0; i--){
+        console.log(mayank[i])
+    }
+}
+sahil()
+
+
+function even(){
+    for(i=0; i<=mayank.length; i++){
+        if(mayank[i]%2===0){
+            console.log(mayank[i],"-----","even")
+        }else{
+            console.log(mayank[i],"-----","odd")
+        }
+    }
+}
+
+even()
+
+function odd(){
+ for(i=0; i<=mayank.length; i++){
+         if(mayank[i]%2!==0){
+             console.log(mayank[i],"-----","odd")
+         }
+    }
+ }
+  odd()
+
+
+
+
